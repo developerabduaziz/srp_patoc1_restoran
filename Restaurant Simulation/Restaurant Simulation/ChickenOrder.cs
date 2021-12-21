fbc1d0cf2a2b86808164abec68f77aac23b6f568
Abduaziz <95034075+developerabduaziz@users.noreply.github.com>
2021-12-21 19:23:25 +0500

    Add files via upload

---
+++ Restaurant Simulation/Restaurant Simulation/ChickenOrder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_Simulation
+{
+    internal class ChickenOrder
+    {
+        private int _quantity;
+
+        public ChickenOrder(int quantity)
+        {
+            _quantity = quantity;
+        }
+
+        int GetQuantity()
+        {
+            return this._quantity;
+        }
+
+
+        void CutUp() 
+        { 
+
+        }
+        void Cook()
+        {
+
+        }
+
+
+    }
+}
